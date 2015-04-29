33f1728c0966913d9a3354e5eca2f422549400e8
Christopher <student@epicodus.com>
2015-04-29 10:22:03 -0700

    add and pass vehicle id from list spec

--- spec/vehicle_integration_spec.rb
+++ spec/vehicle_integration_spec.rb
@@ -14,4 +14,17 @@ describe('the vehicle website path', :type => :feature) do
     click_link('See Vehicle List')
     expect(page).to have_content('Prius')
   end
+  it('adds a vehicle to the list') do
+    visit('/')
+    click_link('Add New Vehicle')
+    fill_in('make', :with => 'Toyota')
+    fill_in('model', :with => 'Corolla')
+    fill_in('year', :with => '1999')
+    click_button('Add Vehicle')
+    click_link('See Vehicle List')
+    click_link('Corolla')
+    expect(page).to have_content('Toyota')
+    expect(page).to have_content('Corolla')
+    expect(page).to have_content('1999')
+  end
 end
